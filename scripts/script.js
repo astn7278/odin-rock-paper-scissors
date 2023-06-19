fb0f78438e8839d82b3fc1325e851b0288601cc3
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2023-06-18 23:35:02 -0700

    Create function to run game and determine winner

--- scripts/script.js
+++ scripts/script.js
@@ -8,56 +8,80 @@ function getComputerChoice() {
             return "Scissors"
         }
 }
+//create random computer selection of rock, paper, scissors.
 
-const computerSelection = getComputerChoice();
+let computerSelection = getComputerChoice();
+//assign getComputerChoice function to variable
 
-let playerSelection = prompt("Do you choose Rock, Paper, or Scissors?");
+let playerSelection;
+//create playerSelection variable
 
-let tieString = "Tie Game. ";
-let winString = "You win! ";
-let loseString = "You lose... ";
+const tieString = "Tie Game. ";
+const winString = "You win! ";
+const loseString = "You lose... ";
+//create win/lose/tie variables
+
+let playerScore = 0;
+let computerScore = 0;
+//create player/computer scores for tracking wins
 
 function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("Do you choose Rock, Paper, or Scissors?");
     if (playerSelection.toLowerCase () === "rock" && computerSelection === "Rock") {
-        return(tieString + "Rock = Rock.")
+        return(tieString + "Rock = Rock.");
     }
-
     else if (playerSelection.toLowerCase () === "paper" && computerSelection === "Paper") {
-        return(tieString + "Paper = Paper.")
+        return(tieString + "Paper = Paper.");
     }
-
     else if (playerSelection.toLowerCase () === "scissors" && computerSelection === "Scissors") {
-        return(tieString + "Scissors = Scissors.")
+        return(tieString + "Scissors = Scissors.");
     }
-
     else if (playerSelection.toLowerCase () === "rock" && computerSelection === "Paper") {
         ++computerScore;
-        return(loseString + "Paper beats Rock.")
+        return(loseString + "Paper beats Rock.");
     }
-
     else if (playerSelection.toLowerCase () === "paper" && computerSelection === "Scissors") {
         ++computerScore;
-        return(loseString + "Scissors beats Paper.")
+        return(loseString + "Scissors beats Paper.");
     }
-
     else if (playerSelection.toLowerCase () === "scissors" && computerSelection === "Rock") {
         ++computerScore;
-        return(loseString + "Rock beats Scissors.")
+        return(loseString + "Rock beats Scissors.");
     }
-
     else if (playerSelection.toLowerCase () === "rock" && computerSelection === "Scissors") {
-        ++userScore;
+        ++playerScore;
         return(winString + "Rock beats Scissors.");
     }
-
     else if (playerSelection.toLowerCase () === "paper" && computerSelection === "Rock") {
-        ++userScore;
+        ++playerScore;
         return(winString + "Paper beats Rock.");      
     }
-
     else if (playerSelection.toLowerCase () === "scissors" && computerSelection === "Paper") {
-        ++userScore;
+        ++playerScore;
         return(winString + "Scissors beats Paper.");
     }
 }
+//game engine comparing playerSelection vs computerSelection 
+//increments playerScore and computerScore
+
+function game() {
+    console.log(playRound(playerSelection, getComputerChoice()));
+    console.log(playRound(playerSelection, getComputerChoice()));
+    console.log(playRound(playerSelection, getComputerChoice()));
+    console.log(playRound(playerSelection, getComputerChoice()));
+    console.log(playRound(playerSelection, getComputerChoice()));
+    if (playerScore > computerScore) {
+        alert("You won the game! Player = " + playerScore + " Computer = " + computerScore)
+    }   else if (playerScore < computerScore) {
+        alert("You lost the game... Player = " + playerScore + " Computer = " + computerScore)
+    }   else {
+        alert("It was a tie game. Player = " + playerScore + " Computer = " + computerScore)
+    }
+}
+//creates 5-round game and compares playerScore and computerScore at end
+
+game();
+//runs game on browser load
+
+    
 
